d8d9e9cf4859c1b9999b3f52dad4809894bed47b
akyssaeduarda <akyssaeduarda.dev@gmail.com>
2023-08-11 11:48:47 -0300

    implementando o duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -3,6 +3,7 @@ from dino_runner.utils.constants import RUNNING,JUMPING,DUCKING
 
 X_POS = 80 
 Y_POS = 310
+Y_POS_DUCK = 345
 JUMP_VEL = 8.5
 
 class Dinosaur: 
@@ -15,19 +16,28 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
     
     def update(self, user_input):
         if self.dino_run:
             self.run()
+        elif self.dino_duck:
+            self.duck()
         elif self.dino_jump:
             self.jump()
         
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN]:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
+        elif not self.dino_jump and not user_input[pygame.K_DOWN]:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
         
         if self.step_index >= 10:
             self.step_index = 0
@@ -51,10 +61,14 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL    
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.step_index += 1
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-         
+
         
 
