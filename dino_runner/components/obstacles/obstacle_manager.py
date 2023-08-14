05700f0e5a5f0dcb3873c7b6b5a6cd66c8528439
akyssaeduarda <akyssaeduarda.dev@gmail.com>
2023-08-14 18:55:58 -0300

    Adição dos obtáculos
    falta fazer o bird bater asas

---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+
+Y_POS_SMALL = 325
+Y_POS_LARGE = 300
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+
+    def update(self, game): 
+        options = random.randint(0, 2)
+        if len(self.obstacles) == 0:
+          if options == 0:
+              self.obstacles.append(Cactus(SMALL_CACTUS , Y_POS_SMALL))
+          elif options == 1:
+              self.obstacles.append(Cactus(LARGE_CACTUS , Y_POS_LARGE))
+          elif options == 2:
+              self.obstacles.append(Bird(BIRD))
+            
+        
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(500)
+                game.playing = False
+                break
+            
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
+           
