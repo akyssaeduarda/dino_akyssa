c225febc8aa01553aace3d639cb14fd0d21af113
akyssaeduarda <akyssaeduarda.dev@gmail.com>
2023-08-14 15:53:06 -0300

    Corrigindo o duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -3,7 +3,7 @@ from dino_runner.utils.constants import RUNNING,JUMPING,DUCKING
 
 X_POS = 80 
 Y_POS = 310
-Y_POS_DUCK = 345
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
 class Dinosaur: 
@@ -30,11 +30,11 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif user_input[pygame.K_DOWN]:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
             self.dino_run = False
             self.dino_duck = True
-        elif not self.dino_jump and not user_input[pygame.K_DOWN]:
+        elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
             self.dino_duck = False
@@ -66,6 +66,7 @@ class Dinosaur:
         self.step_index += 1
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
+        self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
